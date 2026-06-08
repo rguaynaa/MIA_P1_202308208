ec0d0e1cca41c9271259f53f8ec6e377db8d911b
Rodrigo Andres Guay Minera <2990529900101@ingenieria.usac.edu.gt>
2026-06-07 20:31:33 -0600

    carpetas y prueba

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println("BIENVENIDO A LA GESTIÓN DE ARCHIVOS")
+	fmt.Println("¿QUE DESEA HACER?")
+	fmt.Println("1.")
+	fmt.Println("2.")
+	fmt.Println("3.")
+	fmt.Println("4.")
+	fmt.Println("5.")
+	fmt.Println("6.")
+
+	swicth opcion{
+		case:"1"
+		fmt.Println("eligio la opcion1"),
+		case:"2",
+		case:"3",
+		case:"4",
+		case:"5",
+		case:"6"
+	}
+
+
+}
+
+
