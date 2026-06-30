0670032dfacce950a44c9d8b8918a8995ddcdb30
Rodrigo Andres Guay Minera <2990529900101@ingenieria.usac.edu.gt>
2026-06-29 22:21:06 -0600

    aws añadido

--- FASE_2/Frontend/src/api/client.js
+++ FASE_2/Frontend/src/api/client.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'http://3.142.94.44:8080'
 
 async function request(path, options = {}) {
   const res = await fetch(`${BASE_URL}${path}`, {
